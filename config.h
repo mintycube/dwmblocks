bd2594edee18145b982f2c69a5aca98f4f4c3ace
mintycube <90507714+mintycube@users.noreply.github.com>
2024-06-25 16:38:00 +0500

    Add my config

--- config.h
+++ config.h
@@ -2,7 +2,9 @@
 #define CONFIG_H
 
 // String used to delimit block outputs in the status.
-#define DELIMITER "  "
+#define DELIMITER " ^c#37474f^󰄽 ^d^"
+// #define DELIMITER " ^c#37474f^:: ^d^"
+// #define DELIMITER " ^c#37474f^^d^ "
 
 // Maximum number of Unicode characters that a block can output.
 #define MAX_BLOCK_OUTPUT_LENGTH 45
@@ -18,15 +20,10 @@
 
 // Define blocks for the status feed as X(icon, cmd, interval, signal).
 #define BLOCKS(X)             \
-    X("", "sb-mail", 600, 1)  \
-    X("", "sb-music", 0, 2)   \
-    X("", "sb-disk", 1800, 3) \
-    X("", "sb-memory", 10, 4) \
-    X("", "sb-loadavg", 5, 5) \
-    X("", "sb-mic", 0, 6)     \
-    X("", "sb-record", 0, 7)  \
-    X("", "sb-volume", 0, 8)  \
-    X("", "sb-battery", 5, 9) \
-    X("", "sb-date", 1, 10)
+    X("", "cat /tmp/recordingicon 2>/dev/null", 0, 9)  \
+    X("", "sb-internet", 5, 4)   \
+    X("", "sb-volume", 0, 8) \
+    X("", "sb-battery", 5, 21) \
+    X("", "sb-clock", 60, 1)
 
 #endif  // CONFIG_H
